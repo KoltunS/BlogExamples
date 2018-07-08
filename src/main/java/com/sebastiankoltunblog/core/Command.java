ed5c3d371e23f037827a385a4153b106799c8714
skoltun <nr6443041>
2018-07-08 17:21:21 +0200

    modularization

--- src/main/java/com/sebastiankoltunblog/core/Command.java
+++
@@ -1,4 +0,0 @@
-package com.sebastiankoltunblog.core;
-
-public interface Command {
-}
--- src/main/java/com/sebastiankoltunblog/productcart/AddToCartHandler.java
+++ src/main/java/com/sebastiankoltunblog/productcart/AddToCartHandler.java
@@ -1,10 +1,13 @@
 package com.sebastiankoltunblog.productcart;
 
-import com.sebastiankoltunblog.core.CommandHandler;
 
+import com.sebastiankoltunblog.commandhandler.CommandHandler;
+import com.sebastiankoltunblog.dto.AddToCartCommand;
 import org.apache.log4j.Logger;
 
+import javax.inject.Named;
 
+@Named
 public class AddToCartHandler extends CommandHandler<AddToCartCommand, Void> {
     private static final Logger logger = Logger.getLogger(AddToCartHandler.class);
 
